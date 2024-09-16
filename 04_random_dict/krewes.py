0185d8782ef961503dde7c3583ae12b73c77d44a
Tanzeem Hasan <thasan50@stuy.edu>
2024-09-16 10:33:47 -0400

    This should be k4

---
+++ 04_random_dict/krewes.py
@@ -0,0 +1,25 @@
+# Tanzeem Hasan
+# Belugas
+# SoftDev
+# K04 -- Python Random Selection & Dictionaries
+# 2024-9-13
+# Time Spent: 10
+import random
+
+krewes = {
+    4: [
+        'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+    ],
+    5: [
+        'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+        'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+        'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+        'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN'
+    ]
+}
+
+value = random.choice(list(krewes.values()))
+print(random.choice(value))

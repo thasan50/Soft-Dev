5297f33350edb796f97dbf141ee9668accd634a0
thasan50 <thasan3068@gmail.com>
2025-01-10 00:59:30 -0500

    Pushed k27

--- 27_js0/fibfac.js
+++ 27_js0/fibfac.js
@@ -8,10 +8,19 @@
 //factorial:
 
 //<your team's fact(n) implementation>
+function fact(n) {
+    // Base case: factorial of 0 or 1 is 1
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    // Recursive case: n * factorial of (n-1)
+    return n * fact(n - 1);
+}
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fact(5));
+console.log(fact(0));
 
 //-----------------------------------------------------------------
 
@@ -19,8 +28,21 @@
 //fib:
 
 //<your team's fib(n) implementation>
+function fibonacci(n) {
+    // Base case: fibonacci(0) = 0, fibonacci(1) = 1
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    // Recursive case: fibonacci(n) = fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fibonacci(5)); 
+console.log(fibonacci(4)); 
+console.log(fibonacci(0)); 
+console.log(fibonacci(1));
 //=================================================================
